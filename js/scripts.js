2e18e626eaa5f62b593c30a939df9f2d2544f365
lokeshrookie <rocklinglokesh@gmail.com>
2024-05-20 11:29:19 +0530

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// JavaScript file for additional interactivity and animations
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Add any JS code for animations or interactivity
+    console.log('Page loaded and scripts running!');
+});
